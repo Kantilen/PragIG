b9a2c3f4e17b3d1db01486d9cfffbd18a325dd4a
Kantilen <kevin.lamkiewicz@gmail.com>
2016-06-13 13:03:55 +0200

    Included networkx. First approach to implement the cBP

--- build_cBP.py
+++ build_cBP.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import re
+import networkx as nx
 
 __author__ = 'klamkiewicz'
 
@@ -19,3 +20,33 @@ def connect_adjacencies(adjA, adjB):
             else:
                 adjacencies[single_extremity.group(1)] = single_extremity.group(2)
                 adjacencies[single_extremity.group(2)] = single_extremity.group(1)
+
+    create_circular_graph()
+
+def create_circular_graph():
+    G = nx.MultiGraph()
+    G.add_nodes_from(adjacencies_a.keys())
+    visited = []
+    for key, value in adjacencies_a.items():
+        if key in visited or value in visited:
+            continue
+    #TODO: Different telomeres! Currently there is only on telomere node.
+        if not value:
+            if not adjacencies_b[key]:
+                G.add_edge(key, 'Telomere')
+                G.add_edge('Telomere', key)
+            else:
+                G.add_edge(key, 'Telomere')
+        else:
+            visited.append(value)
+            G.add_edge(key,value)
+            if adjacencies_b[key]:
+                G.add_edge(key, adjacencies_b[key])
+            else:
+                G.add_edge(key, 'Telomere')
+            G.add_edge(value, adjacencies_b[value])
+        visited.append(key)
+
+    print G.nodes()
+    print G.edges()
+    print [x for x in nx.connected_components(G)]
