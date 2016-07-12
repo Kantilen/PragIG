4d649f3c86be247784d05f57e968c59dff0380e8
Kantilen <kevin.lamkiewicz@gmail.com>
2016-07-12 13:25:22 +0200

    Added files in src folder

---
+++ src/PragIG.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+
+#################################
+# Import section                #
+#################################
+import argparse as args
+import sys
+
+from input_parser import Input
+from intermediate_genome import Intermediate_Genome
+from genome_sampler import Genome_Sampler
+#################################
+
+
+__author__ = 'klamkiewicz'
+
+# Commandline arguments
+parser = args.ArgumentParser(description="Enter two genomes in the input format of Unimog")
+parser.add_argument('G', metavar='GENOMES', type=str, help="Path to the file that contains the information of extant genomes")
+parser.add_argument('T', metavar='TREE', type=str, help="Path to the file that contains the NEWICK tree")
+
+arguments = parser.parse_args()
+
+# Genome content and tree file are read
+input = Input(arguments.G, arguments.T)
+# Get all pairwise 'siblings' in the tree
+pairwise_genomes = input.find_pairwise_leaves(input.tree)
+
+potential_ancestors = {}
+
+# main iteration
+#TODO: Do a while-loop, pop the first element and update the list with the next (resolved) level in the tree.
+for pair in pairwise_genomes:
+    first_content = input.genomes[pair[0]]
+    second_content = input.genomes[pair[1]]
+
+
+    # Create instance of Intermediate_Genome with the two current sibling-genomes.
+    inter_info = Intermediate_Genome(pair[0], pair[1], first_content, second_content)
+
+    # check if content of genomes is identical.
+    is_valid = inter_info.validate_input()
+
+    # if not, quit the process
+    if not is_valid[0]:
+        print >> sys.stderr, "Content of the genomes is not equal. Check your input!"
+        print >> sys.stderr, " ".join(["%s" % x for x in is_valid[1]])
+        sys.exit(1)
+
+    # Adjacency sets are created
+    inter_info.create_adjacency_sets()
+    # Create the circular breakpoint graph of the two genomes
+    inter_info.create_circular_graph()
+    # Find all intermediate genomes
+    inter_info.get_all_inter_adj()
+    # Create binary vector
+    inter_info.create_binary_vector()
+    # Update everything
+    potential_ancestors.update({(pair[0],pair[1]) : (inter_info.circular_breakpoint, inter_info.inter_adj, inter_info.binaries)})
+
+    sample_genomes = Genome_Sampler(potential_ancestors[(pair[0],pair[1])], 1000)
+
+for key,value in potential_ancestors.items():
+    print key, len(value[1]), value[2]
---
+++ src/genome_sampler.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+__author__ = 'klamkiewicz'
+
+#################################
+# Import section                #
+#################################
+import numpy as np
+#################################
+
+class Genome_Sampler():
+    '''
+    This class generates sampled genomes from the given data.
+    It returns a list of potential ancestral genomes.
+    '''
+    def __init__(self, data, iter):
+        self.breakpoint_graph = data[0]
+        self.all_adjacencies = data[1]
+        self.iteration = iter
+
+        print len(self.all_adjacencies)
